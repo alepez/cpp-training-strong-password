e25370a68a3781484baf71e23ca52fafb8bf22d6
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-11-16 22:20:47 +0100

    add test examples

--- src/strong_password/strong_password_test.cpp
+++ src/strong_password/strong_password_test.cpp
@@ -1,7 +1,11 @@
 #include <catch2/catch_test_macros.hpp>
-
+#include <catch2/matchers/catch_matchers_all.hpp>
 #include <strong_password/strong_password.hpp>
+
+using Catch::Matchers::EndsWith;
 
 TEST_CASE("strong password") {
   REQUIRE(example() == 42);
+  REQUIRE(0.99999 == Catch::Approx(1));
+  REQUIRE_THAT("C++ is magic", EndsWith("magic"));
 }
