e08b1eefad1f9ab2dc94790c26d18509dd3bb3f1
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-11-23 22:51:49 +0100

    Remove strong_password library from dependencies

--- examples/example1/main.cpp
+++ examples/example1/main.cpp
@@ -1,13 +1,11 @@
-#include "strong_password/strong_password.hpp"
-
 #include <fmt/format.h>
 #include <spdlog/spdlog.h>
 
 #include <iostream>
 
 int main(int argc, const char* argv[]) {
-  std::cout << example() << std::endl;
-  fmt::print("{}\n", example());
-  SPDLOG_INFO("{}", example());
+  std::cout << 42 << std::endl;
+  fmt::print("{}\n", 42);
+  SPDLOG_INFO("{}", 42);
   return 0;
 }
--- tests/integration/integration_test.cpp
+++ tests/integration/integration_test.cpp
@@ -1,7 +1,5 @@
 #include <catch2/catch_test_macros.hpp>
 
-#include <strong_password/strong_password.hpp>
-
 TEST_CASE("integration test") {
-  REQUIRE(example() == 42);
+  REQUIRE(42 == 42);
 }
